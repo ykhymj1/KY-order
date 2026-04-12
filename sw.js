11e8dab1002659df68ed3ffeaf908f61d0f230f4
ykhymj1 <91314076+ykhymj1@users.noreply.github.com>
2026-04-12 21:01:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// KY 주문 시스템 Service Worker v8
-const CACHE = 'ky-order-v8';
+// KY 주문 시스템 Service Worker v9
+const CACHE = 'ky-order-v9';
 const ASSETS = [
   '/KY-order/',
   '/KY-order/index.html',
