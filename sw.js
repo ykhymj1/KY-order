6f1ca168efdff9fa453c088eab387fd0b557abf8
ykhymj1 <91314076+ykhymj1@users.noreply.github.com>
2026-04-14 22:18:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// KY 주문 시스템 Service Worker v20260413-hotfix4
-const CACHE = 'ky-order-v20260413-hotfix4';
-const APP_ROOT = '/KY-order';
+// KY 주문 시스템 Service Worker v20260413-netlify1
+const CACHE = 'ky-order-v20260414-netlify1';
+const APP_ROOT = '';  // Netlify 루트 배포
 const ASSETS = [
   APP_ROOT + '/',
   APP_ROOT + '/index.html',
